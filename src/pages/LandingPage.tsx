7e001343dcf0c0952fd7cdef9bc5488c871a4e63
rodger0531 <rodger0531@gmail.com>
2026-02-05 15:27:24 +0800

    feat: greeting and couple img

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,32 @@
+import Couple from '@/assets/couple.svg'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import { useTranslation } from 'react-i18next'
+
 const LandingPage = () => {
-  return <div>LandingPage</div>
+  const { t } = useTranslation(undefined, { keyPrefix: 'landing' })
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      maxWidth="800px"
+      margin="0 auto"
+    >
+      <Grid>
+        <Typography
+          variant="h3"
+          align="center"
+          sx={{ margin: '2rem', fontFamily: 'Dongle' }}
+        >
+          {t('greeting')}
+        </Typography>
+      </Grid>
+      <Grid width="100%" display="flex" justifyContent="center">
+        <img src={Couple} alt="Couple" width="100%" />
+      </Grid>
+    </Grid>
+  )
 }
 
 export default LandingPage
