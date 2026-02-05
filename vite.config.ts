bbeb37a2d34e89b66cfff0877ee0effcdfcf5298
rodger0531 <rodger0531@gmail.com>
2026-02-06 01:39:33 +0800

    feat: use vite-plugin-font for font file size reduction

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 import { defineConfig } from 'vite'
+import Font from 'vite-plugin-font'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Font.vite({
+      scanFiles: ['src/**/*.{ts,tsx,js,jsx}', 'src/i18n/locales/*.json'],
+    }),
+  ],
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
