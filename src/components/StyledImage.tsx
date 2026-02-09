12f6d44c3a19da3364fb0a116deed82adbdc15e8
Rodger <rodger.lo@privetechnologies.com>
2026-02-09 12:30:22 +0800

    fix: move user interaction block to parent

--- src/components/StyledImage.tsx
+++ src/components/StyledImage.tsx
@@ -2,13 +2,17 @@ import Box, { type BoxProps } from '@mui/material/Box'
 
 const StyledImage = ({ sx, ...props }: BoxProps<'img'>) => {
   return (
-    <Box height="100%">
+    <Box
+      height="100%"
+      draggable={false}
+      sx={{
+        userSelect: 'none',
+        pointerEvents: 'none',
+      }}
+    >
       <Box
         component="img"
-        draggable={false}
         sx={{
-          userSelect: 'none',
-          pointerEvents: 'none',
           objectFit: 'contain',
           width: '100%',
           height: '100%',
