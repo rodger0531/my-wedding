aba06b289e060e073e89df96ffb0f7a1baa67326
rodger0531 <rodger0531@gmail.com>
2026-02-11 00:02:08 +0800

    refactor: simplify img component

--- src/components/StyledImage.tsx
+++ src/components/StyledImage.tsx
@@ -3,24 +3,18 @@ import Box, { type BoxProps } from '@mui/material/Box'
 const StyledImage = ({ sx, ...props }: BoxProps<'img'>) => {
   return (
     <Box
-      height="100%"
+      component="img"
       draggable={false}
       sx={{
         userSelect: 'none',
         pointerEvents: 'none',
+        objectFit: 'contain',
+        width: '100%',
+        height: '100%',
+        ...sx,
       }}
-    >
-      <Box
-        component="img"
-        sx={{
-          objectFit: 'contain',
-          width: '100%',
-          height: '100%',
-          ...sx,
-        }}
-        {...props}
-      />
-    </Box>
+      {...props}
+    />
   )
 }
 
